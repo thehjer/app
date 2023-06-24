e4bf485fe27582c5769b8455fc2f2c55eeabe570
thehjer <thehjer9001@gmail.com>
2023-06-24 17:37:49 +0400

    added

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,24 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const contacts =[
+    {
+      id: '1',
+      name:'dipesh',
+      email:'malvesh@gmail.com',
+    },
+    {
+      id: '2',
+      name:'nipesh',
+      email:'nicks@gmail.com',
+    },
+
+  ]
   return (
     <div className='ui container'>
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contacts} />
     </div>
   )
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const ContactList= () =>{
-    return (
-        <div className='ui celled list'>Contact List</div>
-    );
+const ContactList= (props) => {
+    console.log(props);
+
+    const renderContactList =props.contacts.map((contact) => {
+        return(
+            <div className='item'>
+                <div className='content'>
+                    <div className='header'>{contact.name}</div>
+                    <div>{contact.email}</div>
+                    <i className='trash alternate outline icon'></i>
+                </div>
+            </div>        
+            );
+    });
+    return <div className='ui celled list'>{renderContactList}</div>
 };
 
 export default ContactList;
