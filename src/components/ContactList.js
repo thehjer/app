2c42be9edb522ffd575fa485fdb2dcb2fb1c45d2
thehjer <thehjer9001@gmail.com>
2023-06-25 18:06:47 +0400

    modified

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,18 +1,13 @@
 import React from 'react';
+import ContactCard from './ContactCard';
 
 const ContactList= (props) => {
     console.log(props);
 
     const renderContactList =props.contacts.map((contact) => {
-        return(
-            <div className='item'>
-                <div className='content'>
-                    <div className='header'>{contact.name}</div>
-                    <div>{contact.email}</div>
-                    <i className='trash alternate outline icon'></i>
-                </div>
-            </div>        
-            );
+        return <ContactCard contact={contact} />
+                  
+        
     });
     return <div className='ui celled list'>{renderContactList}</div>
 };
